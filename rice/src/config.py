1997d13ed5360ce46a31b30a45c98cca2d950577
Sarunrat <tee20122538@gmail.com>
2026-09-21 22:53:56 +0700

    use densenet121

--- rice/src/config.py
+++ rice/src/config.py
@@ -9,7 +9,7 @@ class RiceConfig:
 
     # 2. Model Architecture (เลือกสถาปัตยกรรมโมเดลที่ต้องการเทรนได้ที่นี่โดยตรง)
     # ตัวเลือกที่รองรับ: "ResNet50V2" | "ConvNeXtTiny" | "DenseNet121" | "EfficientNetV2B0" | "EfficientNetB0" | "EfficientNetB1"
-    BACKBONE = "ConvNeXtTiny"
+    BACKBONE = "DenseNet121"
     WEIGHTS = "imagenet"
     DENSE_UNITS = 64
     DROPOUT_RATE = 0.3
